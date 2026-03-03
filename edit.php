c11817af8ac3f46bcb8b013af1e5803049364be6
Abdulrahman Attia <abdulrahman.attia.dev@email.com>
2026-03-04 00:54:12 +0200

    Add edit user feature

---
+++ edit.php
@@ -0,0 +1,46 @@
+<?php
+require "connection.php";
+
+$id = $_GET['id'];
+
+$stmt = $connection->prepare("SELECT * FROM emp WHERE id = ?");
+$stmt->execute([$id]);
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit User</title>
+</head>
+<body>
+
+<h2>Edit User</h2>
+
+<form action="update.php" method="POST">
+
+    <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+
+    First Name:
+    <input type="text" name="f_name" value="<?php echo $user['f_name']; ?>">
+    <br><br>
+
+    Last Name:
+    <input type="text" name="l_name" value="<?php echo $user['l_name']; ?>">
+    <br><br>
+
+    Email:
+    <input type="text" name="email" value="<?php echo $user['email']; ?>">
+    <br><br>
+
+    Address:
+    <input type="text" name="address" value="<?php echo $user['address']; ?>">
+    <br><br>
+
+    <input type="submit" value="Update">
+
+</form>
+
+</body>
+</html>
